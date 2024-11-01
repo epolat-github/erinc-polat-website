5a8acce84444c5a3834d36422d2d06df8a7c1317
Erinç Polat <51881836+epolat-github@users.noreply.github.com>
2024-11-01 23:55:01 +0300

    Change the design of MyWorkCard

--- src/components/MyWorkCard/index.tsx
+++ src/components/MyWorkCard/index.tsx
@@ -1,6 +1,5 @@
 import spacing from "@/utils/spacing";
-import theme from "@/utils/theme";
-import { Button, Divider, Stack, Typography } from "@mui/material";
+import { Button, ButtonBase, Divider, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import EastRoundedIcon from "@mui/icons-material/EastRounded";
 import { AssetImage } from "@/types/project.type";
@@ -16,86 +15,88 @@ const MyWorkCard: React.FC<MyWorkCardType> = (props) => {
     const { title, description, images, onClick } = props;
 
     return (
-        <Stack
-            sx={{
-                borderColor: theme.palette.grey[200],
-                overflow: "hidden",
-                borderRadius: 4,
-                borderStyle: "solid",
-                height: "100%",
-                maxHeight: 500,
-            }}
-            direction="column"
-        >
+        <ButtonBase onClick={onClick} disableRipple>
             <Stack
                 sx={{
-                    width: "100%",
-                    height: 300,
+                    overflow: "hidden",
+                    height: "100%",
                 }}
-                direction="row"
+                direction="column"
             >
-                {images.map(({ url, alt }, index) => (
-                    <Stack
-                        key={`project-image-${index}`}
-                        sx={{
-                            flex: 1,
-                            position: "relative",
-                        }}
-                    >
-                        <Image
-                            src={url}
-                            alt={alt}
-                            fill
-                            style={{
-                                objectFit: "contain",
+                <Stack
+                    sx={{
+                        width: "100%",
+                        height: 400,
+                        backgroundColor: "#F9F9F9",
+                    }}
+                    direction="row"
+                >
+                    {images.map(({ url, alt }, index) => (
+                        <Stack
+                            key={`project-image-${index}`}
+                            sx={{
+                                flex: 1,
+                                position: "relative",
                             }}
-                        />
-                    </Stack>
-                ))}
-            </Stack>
-
-            <Stack
-                flex={1}
-                justifyContent="space-between"
-                // sx={{
-                //     background: "red",
-                // }}
-            >
-                <Stack gap={spacing.tiny} p={spacing.tiny}>
-                    <Typography variant="h5" fontWeight="bold">
-                        {title}
-                    </Typography>
-                    <Divider
-                        sx={{
-                            backgroundColor: "#ffffff29",
-                            opacity: 0.6,
-                        }}
-                    />
-                    <Typography>{description}</Typography>
+                        >
+                            <Image
+                                src={url}
+                                alt={alt}
+                                fill
+                                style={{
+                                    objectFit: "contain",
+                                }}
+                            />
+                        </Stack>
+                    ))}
                 </Stack>
 
                 <Stack
+                    justifyContent="space-between"
                     sx={{
-                        alignItems: "flex-end",
-                        pr: spacing.tiny,
-                        mb: spacing.tiny / 3,
+                        height: 220,
                     }}
                 >
-                    <Button
-                        variant="text"
-                        color="primary"
-                        endIcon={<EastRoundedIcon />}
+                    <Stack gap={spacing.tiny} p={spacing.tiny}>
+                        <Typography
+                            variant="h5"
+                            fontWeight="bold"
+                            textAlign="start"
+                        >
+                            {title}
+                        </Typography>
+                        <Divider
+                            sx={{
+                                backgroundColor: "#ffffff29",
+                                opacity: 0.6,
+                            }}
+                        />
+                        <Typography textAlign="start">{description}</Typography>
+                    </Stack>
+
+                    <Stack
                         sx={{
-                            fontSize: 16,
-                            background: "transparent",
+                            alignItems: "flex-end",
+                            pr: spacing.tiny,
+                            mb: spacing.tiny / 3,
                         }}
-                        onClick={onClick}
                     >
-                        Explore work
-                    </Button>
+                        <Button
+                            component={"div"}
+                            variant="text"
+                            color="primary"
+                            endIcon={<EastRoundedIcon />}
+                            sx={{
+                                fontSize: 16,
+                                background: "transparent",
+                            }}
+                        >
+                            Explore work
+                        </Button>
+                    </Stack>
                 </Stack>
             </Stack>
-        </Stack>
+        </ButtonBase>
     );
 };
 
