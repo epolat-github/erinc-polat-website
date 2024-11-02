a6a274d6e5bf7df07dcc136905294d794f67d8b5
Erinç Polat <51881836+epolat-github@users.noreply.github.com>
2024-11-03 02:44:44 +0300

    Add animation to the welcome section

--- src/components/WelcomeSection/index.tsx
+++ src/components/WelcomeSection/index.tsx
@@ -14,6 +14,8 @@ import spacing from "@/utils/spacing";
 import DotPattern from "../DotPattern";
 import theme from "@/utils/theme";
 import ProjectsOverview from "./ProjectsOverview";
+import RevealItem from "../RevealItem";
+import FadeInItem from "../FadeInItem";
 
 const WelcomeSection = () => {
     const matchesXsUp = useMediaQuery((theme: Theme) =>
@@ -41,91 +43,108 @@ const WelcomeSection = () => {
                     },
                 }}
             >
-                <Stack flex={0.6} spacing={spacing.small} sx={{}}>
+                <Stack flex={0.6} spacing={spacing.small}>
                     <Stack
                         sx={{
                             position: "relative",
                         }}
                     >
                         <DotPattern />
-                        <Typography
-                            color={colors.header}
-                            fontWeight="500"
-                            fontSize="2rem"
-                            sx={{
-                                zIndex: 1,
-                            }}
-                        >{`Hey there, I'm-`}</Typography>
-                        <Typography
-                            fontWeight="bold"
-                            color={colors.primary}
-                            sx={{
-                                zIndex: 1,
-                                fontSize: {
-                                    lg: "8rem",
-                                    sm: "4rem",
-                                    xs: "3rem",
-                                },
-                            }}
-                        >
-                            Erinç Polat.
-                        </Typography>
+                        <FadeInItem>
+                            <Typography
+                                color={colors.header}
+                                fontWeight="500"
+                                fontSize="2rem"
+                                sx={{
+                                    zIndex: 1,
+                                }}
+                            >{`Hey there, I'm-`}</Typography>
+                        </FadeInItem>
+                        <FadeInItem delay={0.2}>
+                            <Typography
+                                fontWeight="bold"
+                                color={colors.primary}
+                                sx={{
+                                    zIndex: 1,
+                                    fontSize: {
+                                        lg: "8rem",
+                                        sm: "4rem",
+                                        xs: "3rem",
+                                    },
+                                }}
+                            >
+                                Erinç Polat.
+                            </Typography>
+                        </FadeInItem>
                     </Stack>
 
-                    <Typography
-                        variant="h5"
-                        fontWeight="500"
-                        color={colors.gray}
-                    >
-                        <span
-                            style={{
-                                color: colors.header,
-                            }}
+                    <FadeInItem delay={0.3}>
+                        <Typography
+                            variant="h5"
+                            fontWeight="500"
+                            color={colors.gray}
                         >
-                            Software Engineer.
-                        </span>{" "}
-                        A full-stack developer with an interest in developing
-                        products.
-                    </Typography>
+                            <span
+                                style={{
+                                    color: colors.header,
+                                }}
+                            >
+                                Software Engineer.
+                            </span>{" "}
+                            A full-stack developer with an interest in
+                            developing products.
+                        </Typography>
+                    </FadeInItem>
 
                     <Stack spacing={spacing.tiny}>
-                        <Typography variant="h6" color={colors.gray}>
-                            🚀 Customer-tailored websites
-                        </Typography>
-                        <Typography variant="h6" color={colors.gray}>
-                            📱 Cross-platform mobile applications
-                        </Typography>
-                        <Typography variant="h6" color={colors.gray}>
-                            ⚡️ Backend development
-                        </Typography>
+                        <FadeInItem delay={0.4}>
+                            <Typography variant="h6" color={colors.gray}>
+                                🚀 Customer-tailored websites
+                            </Typography>
+                        </FadeInItem>
+                        <FadeInItem delay={0.5}>
+                            <Typography variant="h6" color={colors.gray}>
+                                📱 Cross-platform mobile applications
+                            </Typography>
+                        </FadeInItem>
+                        <FadeInItem delay={0.6}>
+                            <Typography variant="h6" color={colors.gray}>
+                                ⚡️ Backend development
+                            </Typography>
+                        </FadeInItem>
                     </Stack>
 
                     <Stack direction="row" spacing={spacing.small}>
-                        <Button
-                            variant="contained"
-                            sx={{
-                                backgroundColor: colors.primary,
-                                color: "#fff",
-                                minWidth: 150,
-                                height: 40,
-                            }}
-                            disableElevation
-                            href="#contact"
-                        >
-                            Inquire Now
-                        </Button>
-                        <Button
-                            variant="contained"
-                            sx={{
-                                backgroundColor: theme.palette.grey[100],
-                                minWidth: 150,
-                                height: 40,
-                            }}
-                            disableElevation
-                            href="#about"
-                        >
-                            See More
-                        </Button>
+                        <RevealItem delay={0.7}>
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    backgroundColor: colors.primary,
+                                    color: "#fff",
+                                    minWidth: 150,
+                                    height: 40,
+                                }}
+                                disableElevation
+                                href="#contact"
+                            >
+                                Inquire Now
+                            </Button>
+                        </RevealItem>
+
+                        <RevealItem delay={0.9}>
+                            <Button
+                                variant="contained"
+                                sx={{
+                                    backgroundColor: theme.palette.grey[100],
+                                    minWidth: 150,
+                                    height: 40,
+                                }}
+                                disableElevation
+                                href="#about"
+                            >
+                                See More
+                            </Button>
+                        </RevealItem>
                     </Stack>
                 </Stack>
 
@@ -141,7 +160,7 @@ const WelcomeSection = () => {
                             md: 0,
                         },
                         display: {
-                            xs: "none",
+                            // xs: "none",
                             sm: "flex",
                         },
                     }}
