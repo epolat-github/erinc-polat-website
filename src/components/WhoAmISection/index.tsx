0e484a2a8b56d87f5520267b3066bee9157bc05b
Erinç Polat <51881836+epolat-github@users.noreply.github.com>
2025-02-19 11:19:27 +0300

    Translate WhoAmI section

--- src/components/WhoAmISection/index.tsx
+++ src/components/WhoAmISection/index.tsx
@@ -7,8 +7,11 @@ import spacing from "@/utils/spacing";
 import DotPattern from "../DotPattern";
 import FadeInItem from "../FadeInItem";
 import RevealItem from "../RevealItem";
+import { useTranslations } from "next-intl";
 
 const WhoAmISection = () => {
+    const t = useTranslations("who_am_i_section");
+
     return (
         <Box
             id="about"
@@ -55,79 +58,26 @@ const WhoAmISection = () => {
                                         zIndex: 2,
                                     }}
                                 >
-                                    Who am I?
+                                    {t("title")}
                                 </Typography>
                             </RevealItem>
-
-                            {/* <Box
-                                component={motion.div}
-                                initial={{
-                                    width: 0,
-                                }}
-                                whileInView={{
-                                    width: 80,
-                                }}
-                                transition={{
-                                    ease: "easeOut",
-                                    delay: 0.3,
-                                    duration: 0.4,
-                                }}
-                                sx={{
-                                    // width: 100,
-                                    height: 45,
-                                    backgroundColor: theme.palette.grey[200],
-                                    position: "absolute",
-                                    zIndex: 1,
-                                    borderTopRightRadius: spacing.tiny,
-                                    borderBottomRightRadius: spacing.tiny,
-                                }}
-                            /> */}
                         </Stack>
 
                         <Stack spacing={spacing.tiny}>
                             <FadeInItem delay={0.1}>
                                 <Typography color={colors.gray}>
-                                    Hello, I’m Erinç Tuna Polat. I studied
-                                    Computer Engineering at TED University.
-                                    Alongside my major, I pursued a minor in
-                                    psychology to better understand the
-                                    interaction between humans and technology. I
-                                    have been working in the software
-                                    development field for over seven years, and
-                                    I founded my own company to undertake a wide
-                                    variety of projects.
+                                    {t("first_paragraph")}
                                 </Typography>
                             </FadeInItem>
                             <FadeInItem delay={0.2}>
                                 <Typography color={colors.gray}>
-                                    I have led various SaaS projects, including
-                                    IoT device integration, smart lock
-                                    management, IP camera-based security
-                                    systems, and systems that manage building
-                                    access permissions. I developed solutions
-                                    for both web and mobile platforms using
-                                    React/Next.js, React Native/Expo and Nodejs.
-                                    I also optimized projects by managing Docker
-                                    and CI/CD processes, ensuring seamless
-                                    integration and continuous deployment.
+                                    {t("second_paragraph")}
                                 </Typography>
                             </FadeInItem>
 
                             <FadeInItem delay={0.4}>
                                 <Typography color={colors.gray}>
-                                    Among my freelance projects are innovative
-                                    solutions like event management
-                                    applications, contactless receipt systems,
-                                    and fitness assistant apps. I have completed
-                                    projects for clients from Germany, Canada,
-                                    and Türkiye across various sectors. With my
-                                    strong technical expertise and the
-                                    collaborative work I have done with my
-                                    teams, I strive to make a difference in the
-                                    tech world. I look forward to collaborating
-                                    with potential clients on new projects,
-                                    helping them accelerate their digital
-                                    transformation processes.
+                                    {t("third_paragraph")}
                                 </Typography>
                             </FadeInItem>
                         </Stack>
