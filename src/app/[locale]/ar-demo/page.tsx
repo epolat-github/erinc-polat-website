579cabd9b1f01a64c5a599e30d66bc0609b82090
Erinç Polat <51881836+epolat-github@users.noreply.github.com>
2025-08-07 08:57:42 +0300

    Add new 3d asset

--- src/app/[locale]/ar-demo/page.tsx
+++ src/app/[locale]/ar-demo/page.tsx
@@ -29,7 +29,7 @@ const ArDemo = () => {
                 }}
                 spacing={spacing.small}
             >
-                <a id="ar-viewer" rel="ar" href="/wedding_ring.usdz">
+                <a id="ar-viewer" rel="ar" href="/wedding_ring_2.usdz">
                     <Image
                         fill
                         style={{
